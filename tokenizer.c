64faba492fa10bb654937c889701f24d3bb589fa
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-02-26 16:43:27 -0300

    feat(tokenizer): improve token parsing to handle special characters and quoted strings

--- tokenizer.c
+++ tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 00:49:55 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/02/26 14:38:05 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/02/26 16:41:57 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static t_token	*new_token(char *value, t_token_type type)
 	token = malloc(sizeof(t_token));
 	if (!token)
 		return (NULL);
-	token->value = ft_strdup(value);
+	token->value = value;
 	token->type = type;
 	token->next = NULL;
 	return (token);
@@ -43,19 +43,75 @@ static void	add_token(t_token **tokens, t_token *new)
 t_token	*tokenize_input(char *input)
 {
 	t_token	*tokens;
-	char	**parts;
 	int		i;
+	int		start;
+	char	*word;
 
 	tokens = NULL;
-	parts = ft_split(input, ' ');
-	if (!parts)
-		return (NULL);
 	i = 0;
-	while (parts[i])
+	while (input[i])
 	{
-		add_token(&tokens, new_token(parts[i], WORD));
-		i++;
+		while (input[i] && (input[i] == ' ' || input[i] == '\t'))
+			i++;
+		if (!input[i])
+			break ;
+		if (input[i] == '|' || input[i] == '<' || input[i] == '>')
+		{
+			if (input[i] == '|')
+				add_token(&tokens, new_token(ft_strdup("|"), PIPE));
+			else if (input[i] == '<')
+			{
+				if (input[i + 1] == '<')
+				{
+					add_token(&tokens, new_token(ft_strdup("<<"), HEREDOC));
+					i++;
+				}
+				else
+					add_token(&tokens, new_token(ft_strdup("<"), REDIRECT_IN));
+			}
+			else if (input[i] == '>')
+			{
+				if (input[i + 1] == '>')
+				{
+					add_token(&tokens, new_token(ft_strdup(">>"), APPEND));
+					i++;
+				}
+				else
+					add_token(&tokens, new_token(ft_strdup(">"), REDIRECT_OUT));
+			}
+			i++;
+		}
+		else if (input[i] == '\'')
+		{
+			i++;
+			start = i;
+			while (input[i] && input[i] != '\'')
+				i++;
+			word = ft_substr(input, start, i - start);
+			add_token(&tokens, new_token(word, WORD));
+			if (input[i] == '\'')
+				i++;
+		}
+		else if (input[i] == '\"')
+		{
+			i++;
+			start = i;
+			while (input[i] && input[i] != '\"')
+				i++;
+			word = ft_substr(input, start, i - start);
+			add_token(&tokens, new_token(word, WORD));
+			if (input[i] == '\"')
+				i++;
+		}
+		else
+		{
+			start = i;
+			while (input[i] && input[i] != ' ' && input[i] != '\t'
+				&& input[i] != '|' && input[i] != '<' && input[i] != '>')
+				i++;
+			word = ft_substr(input, start, i - start);
+			add_token(&tokens, new_token(word, WORD));
+		}
 	}
-	free(parts);
 	return (tokens);
 }
