f662b8e232750791cf5ea8f66714eba6fae73e35
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-02-26 14:50:51 -0300

    feat(parser): add parse_tokens function to generate command structure from token list

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 14:40:39 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/02/26 14:41:30 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/02/26 14:49:29 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,37 @@ static int	count_tokens(t_token *tokens)
 		tokens = tokens->next;
 	}
 	return (count);
+}
+
+t_command	*parse_tokens(t_token *tokens)
+{
+	t_command	*cmd;
+	char		**args;
+	int			count;
+	int			i;
+	t_token		*tmp;
+
+	cmd = malloc(sizeof(t_command));
+	if (!cmd)
+		return (NULL);
+	count = count_tokens(tokens);
+	args = malloc(sizeof(char *) * (count + 1));
+	if (!args)
+	{
+		free(cmd);
+		return (NULL);
+	}
+	i = 0;
+	tmp = tokens;
+	while (tmp)
+	{
+		args[i] = ft_strdup(tmp->value);
+		i++;
+		tmp = tmp->next;
+	}
+	args[i] = NULL;
+	cmd->args = args;
+	cmd->argc = count;
+	cmd->next = NULL;
+	return (cmd);
 }
