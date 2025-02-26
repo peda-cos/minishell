fe144255ce3556231608e39bcb21c1eb6c18670d
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-02-26 17:55:31 -0300

    refactor(minishell): update header organization with new command redirection fields, variable expansion utility, and signal handling declaration

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 19:15:26 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/02/26 16:49:03 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/02/26 17:55:19 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@
 # include <sys/wait.h>
 # include <unistd.h>
 
+/* Token types */
 typedef enum e_token_type
 {
 	WORD,
@@ -30,10 +31,10 @@ typedef enum e_token_type
 	REDIRECT_IN,
 	REDIRECT_OUT,
 	HEREDOC,
-	APPEND,
-	END
+	APPEND
 }						t_token_type;
 
+/* Token linked list */
 typedef struct s_token
 {
 	char				*value;
@@ -41,26 +42,34 @@ typedef struct s_token
 	struct s_token		*next;
 }						t_token;
 
+/* Command structure: a command is one segment (separated by pipes) */
 typedef struct s_command
 {
 	char				**args;
 	int					argc;
+	char				*input_file;
+	char				*output_file;
+	int					append;
+	char				*heredoc_delim;
 	struct s_command	*next;
 }						t_command;
 
-/* Environment utilities */
+/* Environment management (passed locally) */
 char					**copy_env(char **envp);
 void					free_env(char **env);
 
-/* Tokenizer and Parser */
+/* Tokenizer: splits input into tokens (handling quotes and special symbols) */
 t_token					*tokenize_input(char *input);
+
+/* Parser: builds a linked list of commands from tokens */
 t_command				*parse_tokens(t_token *tokens);
 
-/* Executor */
-void					execute_command(t_command *cmd, char **env);
+/* Executor: executes a command chain (with redirections/pipes) */
+void					execute_command(t_command *cmd, char **env,
+							int *last_exit);
 
-/* Built-in commands */
-int						builtin_echo(char **args);
+/* Builtin commands */
+int						builtin_echo(char **args, int *last_exit);
 int						builtin_cd(char **args);
 int						builtin_pwd(void);
 int						builtin_export(char **args, char ***env);
@@ -68,8 +77,14 @@ int						builtin_unset(char **args, char ***env);
 int						builtin_env(char **env);
 int						builtin_exit(char **args);
 
+/* Signal handling */
+void					signal_handler(int signo);
+
 /* Memory cleanup */
 void					free_tokens(t_token *tokens);
 void					free_commands(t_command *cmd);
+
+/* Utility for variable expansion */
+char					*expand_variables(char *str, char **env, int last_exit);
 
 #endif
