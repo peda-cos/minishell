930bf27c8871a5fc57086bc0ec84ecbbfbc2bfcc
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-02-26 14:55:31 -0300

    feat(utils): add free_tokens function for memory cleanup of token list

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 19:15:26 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/02/26 14:51:12 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/02/26 14:54:46 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,5 +52,7 @@ typedef struct s_command
 
 t_token					*tokenize_input(char *input);
 t_command				*parse_tokens(t_token *tokens);
+
+void					free_tokens(t_token *tokens);
 
 #endif
---
+++ utils.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/26 14:54:34 by peda-cos          #+#    #+#             */
+/*   Updated: 2025/02/26 14:54:36 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	free_tokens(t_token *tokens)
+{
+	t_token	*tmp;
+
+	while (tokens)
+	{
+		tmp = tokens;
+		tokens = tokens->next;
+		free(tmp->value);
+		free(tmp);
+	}
+}
