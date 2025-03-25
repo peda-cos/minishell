53574e5e3cd766503abe2a1fcf4e84caa54765ca
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-03-25 01:57:31 -0300

    refactor(parser): modularize parser implementation by extracting helper functions

--- sources/parser/main.c
+++ sources/parser/main.c
@@ -3,126 +3,91 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlacerda <jlacerda@student.42.fr>          +#+  +:+       +#+        */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 14:40:39 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/03/22 18:44:27 by jlacerda         ###   ########.fr       */
+/*   Updated: 2025/03/25 01:51:38 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "utils.h"
 
-static t_command	*new_command(void)
+static int	is_redirection(t_token_type type)
 {
-	t_command	*cmd;
+	return (type == REDIRECT_IN || type == REDIRECT_OUT || type == APPEND
+		|| type == HEREDOC);
+}
 
-	cmd = malloc(sizeof(t_command));
-	if (!cmd)
-		return (NULL);
-	cmd->args = NULL;
-	cmd->argc = 0;
-	cmd->input_file = NULL;
-	cmd->output_file = NULL;
-	cmd->append = 0;
-	cmd->heredoc_delim = NULL;
-	cmd->next = NULL;
-	return (cmd);
+static void	set_redirection_target(t_command *cmd, t_token *token,
+		t_token *target)
+{
+	if (token->type == REDIRECT_IN)
+		cmd->input_file = ft_strdup(target->value);
+	else if (token->type == REDIRECT_OUT)
+	{
+		cmd->output_file = ft_strdup(target->value);
+		cmd->append = 0;
+	}
+	else if (token->type == APPEND)
+	{
+		cmd->output_file = ft_strdup(target->value);
+		cmd->append = 1;
+	}
+	else if (token->type == HEREDOC)
+		cmd->heredoc_delim = ft_strdup(target->value);
+}
+
+static int	handle_redirection(t_command *cmd, t_token **token_ptr)
+{
+	t_token	*token;
+
+	token = *token_ptr;
+	*token_ptr = token->next;
+	if (!*token_ptr || (*token_ptr)->type != WORD)
+	{
+		ft_putstr_fd("minishell: syntax error near unexpected token\n",
+			STDERR_FILENO);
+		return (0);
+	}
+	set_redirection_target(cmd, token, *token_ptr);
+	*token_ptr = (*token_ptr)->next;
+	return (1);
+}
+
+static t_token	*process_token(t_token *token, t_parser_context *parser)
+{
+	if (!parser->current)
+	{
+		parser->current = append_command(&parser->head);
+		if (!parser->current)
+			return (NULL);
+	}
+	if (token->type == PIPE)
+	{
+		parser->current = NULL;
+		return (token->next);
+	}
+	if (is_redirection(token->type))
+	{
+		if (!handle_redirection(parser->current, &token))
+			return (token);
+		return (token);
+	}
+	if (token->type == WORD)
+		return (process_word_tokens(parser->current, token, parser->env,
+				parser->last_exit));
+	return (token->next);
 }
 
 t_command	*parse_tokens(t_token *tokens, char **env, int last_exit)
 {
-	t_command	*head;
-	t_command	*current;
-	t_token		*tmp;
-	int			count;
-	int			i;
-	t_command	*iter;
+	t_parser_context	parser;
 
-	head = NULL;
-	current = NULL;
+	parser.head = NULL;
+	parser.current = NULL;
+	parser.env = env;
+	parser.last_exit = last_exit;
 	while (tokens)
-	{
-		if (!current)
-		{
-			current = new_command();
-			if (!head)
-				head = current;
-			else
-			{
-				iter = head;
-				while (iter->next)
-					iter = iter->next;
-				iter->next = current;
-			}
-		}
-		if (tokens->type == PIPE)
-		{
-			current = NULL;
-			tokens = tokens->next;
-			continue ;
-		}
-		if (tokens->type == REDIRECT_IN)
-		{
-			tokens = tokens->next;
-			if (!tokens || tokens->type != WORD)
-			{
-				fprintf(stderr,
-					"minishell: syntax error near unexpected token\n");
-				return (NULL);
-			}
-			current->input_file = ft_strdup(tokens->value);
-		}
-		else if (tokens->type == REDIRECT_OUT)
-		{
-			tokens = tokens->next;
-			if (tokens && tokens->type == WORD)
-			{
-				current->output_file = ft_strdup(tokens->value);
-				current->append = 0;
-			}
-		}
-		else if (tokens->type == APPEND)
-		{
-			tokens = tokens->next;
-			if (tokens && tokens->type == WORD)
-			{
-				current->output_file = ft_strdup(tokens->value);
-				current->append = 1;
-			}
-		}
-		else if (tokens->type == HEREDOC)
-		{
-			tokens = tokens->next;
-			if (tokens && tokens->type == WORD)
-				current->heredoc_delim = ft_strdup(tokens->value);
-		}
-		else if (tokens->type == WORD)
-		{
-			tmp = tokens;
-			count = 0;
-			while (tmp && tmp->type == WORD)
-			{
-				count++;
-				tmp = tmp->next;
-				if (tmp && tmp->type != WORD)
-					break ;
-			}
-			current->args = malloc(sizeof(char *) * (count + 1));
-			if (!current->args)
-				return (head);
-			i = 0;
-			while (i < count && tokens && tokens->type == WORD)
-			{
-				current->args[i] = expand_variables(tokens->value, env,
-						last_exit);
-				i++;
-				tokens = tokens->next;
-			}
-			current->args[i] = NULL;
-			current->argc = count;
-			continue ;
-		}
-		tokens = tokens->next;
-	}
-	return (head);
+		tokens = process_token(tokens, &parser);
+	return (parser.head);
 }
---
+++ sources/parser/utils.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/25 01:33:23 by peda-cos          #+#    #+#             */
+/*   Updated: 2025/03/25 01:53:21 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_H
+# define UTILS_H
+
+# include "minishell.h"
+
+/*
+** Parser context structure to reduce parameter count in functions
+*/
+typedef struct s_parser_context
+{
+	t_command	*head;
+	t_command	*current;
+	char		**env;
+	int			last_exit;
+}				t_parser_context;
+
+/*
+** Appends a new command to the end of the command chain
+** Returns the newly added command
+*/
+t_command		*append_command(t_command **head);
+
+/*
+** Processes a sequence of word tokens into command arguments
+** Returns the updated token pointer
+*/
+t_token			*process_word_tokens(t_command *cmd, t_token *token, char **env,
+					int last_exit);
+
+#endif
