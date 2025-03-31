2e31645b8208a4f3eb91acb06a04595e6f1249ff
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-03-31 20:53:14 -0300

    fix(main): update function calls and fix memory management

--- sources/main.c
+++ sources/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlacerda <jlacerda@student.42.fr>          +#+  +:+       +#+        */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 19:05:27 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/03/30 02:03:35 by jlacerda         ###   ########.fr       */
+/*   Updated: 2025/03/31 20:41:44 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,39 +31,41 @@ int	main(int argc, char **argv, char **envp)
 		input = readline("minishell$ ");
 		if (handle_eof(input))
 			break ;
-		if (*input)
-			add_history(input);
-		g_signal_received = 0;
-		tokens = tokenize_input(input);
-		if (tokens)
+		if (input && *input)
 		{
-			cmd = parse_tokens(tokens, env, last_exit);
-			if (cmd)
+			add_history(input);
+			g_signal_received = 0;
+			tokens = tokenize_input(input);
+			if (tokens)
 			{
-				if (!cmd->next && is_parent_builtin(cmd->args[0]))
+				cmd = parse_tokens(tokens, env, last_exit);
+				if (cmd)
 				{
-					if (!ft_strcmp(cmd->args[0], "cd"))
-						last_exit = builtin_cd(cmd->args);
-					else if (!ft_strcmp(cmd->args[0], "export"))
-						last_exit = builtin_export(cmd->args, &env);
-					else if (!ft_strcmp(cmd->args[0], "unset"))
-						last_exit = builtin_unset(cmd->args, &env);
-					else if (!ft_strcmp(cmd->args[0], "exit"))
-						last_exit = builtin_exit(cmd->args);
-					free_commands(cmd);
+					if (!cmd->next && is_parent_builtin(cmd->args[0]))
+					{
+						if (!ft_strcmp(cmd->args[0], "cd"))
+							last_exit = builtin_cd(cmd->args, &env);
+						else if (!ft_strcmp(cmd->args[0], "export"))
+							last_exit = builtin_export(cmd->args, &env);
+						else if (!ft_strcmp(cmd->args[0], "unset"))
+							last_exit = builtin_unset(cmd->args, &env);
+						else if (!ft_strcmp(cmd->args[0], "exit"))
+							last_exit = builtin_exit(cmd->args);
+						free_commands(cmd);
+					}
+					else
+					{
+						saved_stdin = dup(STDIN_FILENO);
+						setup_execution_signals();
+						execute_command(cmd, env, &last_exit, tokens);
+						setup_interactive_signals();
+						dup2(saved_stdin, STDIN_FILENO);
+						close(saved_stdin);
+						free_commands(cmd);
+					}
 				}
-				else
-				{
-					saved_stdin = dup(STDIN_FILENO);
-					setup_execution_signals();
-					execute_command(cmd, env, &last_exit, tokens);
-					setup_interactive_signals();
-					dup2(saved_stdin, STDIN_FILENO);
-					close(saved_stdin);
-					free_commands(cmd);
-				}
+				free_tokens(tokens);
 			}
-			free_tokens(tokens);
 		}
 		free(input);
 	}
