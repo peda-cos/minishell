3fe6e76b5ec5d2d36aababb64e1539c220712dac
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-03-31 20:50:55 -0300

    ix(exit): implement proper numeric validation and exit code handling

--- sources/builtin/exit.c
+++ sources/builtin/exit.c
@@ -3,23 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlacerda <jlacerda@student.42.fr>          +#+  +:+       +#+        */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 14:21:26 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/03/30 00:58:08 by jlacerda         ###   ########.fr       */
+/*   Updated: 2025/03/31 16:37:34 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
+static int	is_numeric_arg(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int	builtin_exit(char **args)
 {
 	int	exit_code;
 
+	ft_putendl_fd("exit", STDOUT_FILENO);
 	exit_code = 0;
-	if (args[1])
-		exit_code = ft_atoi(args[1]);
-	return (exit_code);
+	if (!args[1])
+		return (exit_code);
+	if (!is_numeric_arg(args[1]))
+	{
+		ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+		ft_putstr_fd(args[1], STDERR_FILENO);
+		ft_putendl_fd(": numeric argument required", STDERR_FILENO);
+		return (255);
+	}
+	exit_code = ft_atoi(args[1]);
+	if (args[2])
+	{
+		ft_putendl_fd("minishell: exit: too many arguments", STDERR_FILENO);
+		return (1);
+	}
+	return (exit_code % 256);
 }
 
 int	is_parent_builtin(char *cmd)
