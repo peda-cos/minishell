556cd52f82a27b864a442d0c32609c09c20392de
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-03-31 20:52:21 -0300

    fix(builtin.h): update function signatures to match implementation changes

--- sources/builtin/builtin.h
+++ sources/builtin/builtin.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlacerda <jlacerda@student.42.fr>          +#+  +:+       +#+        */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 14:15:36 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/03/29 20:12:06 by jlacerda         ###   ########.fr       */
+/*   Updated: 2025/03/31 16:42:12 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "minishell.h"
 
-# define UNSET_NO_ARGS_ERR	"unset: not enough arguments"
+# define UNSET_NO_ARGS_ERR "unset: not enough arguments"
 
 /* Environment management */
 char	**copy_env(char **envp);
@@ -23,12 +23,15 @@ int		find_env_index(char *key, char **env);
 
 /* Builtin commands */
 int		builtin_echo(char **args, int *last_exit);
-int		builtin_cd(char **args);
+int		builtin_cd(char **args, char ***env);
 int		builtin_pwd(void);
 int		builtin_export(char **args, char ***env);
 int		builtin_unset(char **args, char ***env);
 int		builtin_env(char **env);
 int		builtin_exit(char **args);
 int		is_parent_builtin(char *cmd);
+
+/* Utility functions */
+char	*ft_strjoin_char(char *str, char c);
 
 #endif
