3dcf7c9f20a4dea87fd9ba9436b4c2727efaf0b8
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-03-31 22:47:56 -0300

    refactor(minishell): reorganize global header file includes and definitions

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,31 +6,28 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 19:15:26 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/03/31 21:18:38 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/03/31 22:15:54 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "../sources/handler/signals.h"
 # include "libft.h"
 # include "macros.h"
 # include "structs.h"
+# include "../sources/handler/signals.h"
+# include "../sources/builtin/builtin.h"
+# include "../sources/expansion/expansion.h"
+
 # include <fcntl.h>
+# include <stdio.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/wait.h>
 # include <readline/history.h>
 # include <readline/readline.h>
-# include <signal.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <sys/wait.h>
-# include <unistd.h>
-
-typedef struct s_expansion_ctx
-{
-	char	**env;
-	int		last_exit;
-}			t_expansion_ctx;
 
 /* Environment management */
 char		**copy_env(char **envp);
@@ -42,16 +39,6 @@ t_command	*parse_tokens(t_token *tokens, char **env, int last_exit);
 /* Executor: executes a command chain */
 void		execute_command(t_command *cmd, char **env, int *last_exit,
 				t_token *tokens);
-
-/* Builtin commands */
-int			builtin_pwd(void);
-int			builtin_env(char **env);
-int			builtin_cd(char **args, char ***env);
-int			builtin_exit(char **args);
-int			is_parent_builtin(char *cmd);
-int			builtin_unset(char **args, char ***env);
-int			builtin_export(char **args, char ***env);
-int			builtin_echo(char **args, int *last_exit);
 
 /* Signal handling */
 void		signal_handler(int signo);
