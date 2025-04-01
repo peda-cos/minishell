b7d7c60d7387cbb1e35b5ab4db0ccf927b34cc27
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-03-31 22:47:56 -0300

    refactor(executor): simplify and clarify process management logic

--- sources/executor/process.c
+++ sources/executor/process.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   process.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlacerda <jlacerda@student.42.fr>          +#+  +:+       +#+        */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 08:19:21 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/03/30 02:07:26 by jlacerda         ###   ########.fr       */
+/*   Updated: 2025/03/31 21:56:29 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
 
-static void exit_child_process(int signal, t_process_command_args *param)
+static void	exit_child_process(int signal, t_process_command_args *param)
 {
 	free_env(param->env);
 	free_commands(param->cmd);
@@ -20,30 +20,10 @@ static void exit_child_process(int signal, t_process_command_args *param)
 	exit(signal);
 }
 
-int	setup_pipe(t_command *cmd, int pipefd[2])
-{
-	if (!cmd)
-		return (-1);
-	if (cmd->next)
-	{
-		if (pipe(pipefd) < 0)
-		{
-			perror("pipe");
-			return (-1);
-		}
-	}
-	else
-	{
-		pipefd[0] = -1;
-		pipefd[1] = -1;
-	}
-	return (0);
-}
-
-void	child_process(t_process_command_args *param) 
+void	child_process(t_process_command_args *param)
 {
 	if (!param->cmd || !param->env || !(param->last_exit))
-	exit_child_process(1, param);
+		exit_child_process(1, param);
 	reset_signals();
 	if (param->cmd->next && param->pipefd[1] > 0)
 	{
@@ -51,16 +31,15 @@ void	child_process(t_process_command_args *param)
 		close(param->pipefd[0]);
 		close(param->pipefd[1]);
 	}
-	if (setup_input_redirection(param->cmd) < 0 
+	if (setup_input_redirection(param->cmd) < 0
 		|| setup_output_redirection(param->cmd) < 0)
-	exit_child_process(1, param);
+		exit_child_process(1, param);
 	if (param->cmd->argc <= 0)
-	exit_child_process(0, param);
+		exit_child_process(0, param);
 	if (is_builtin(param->cmd->args[0]))
-		exit_child_process(execute_builtin(
-			param->cmd, &param->env, (param->last_exit)), param);
-	exit_child_process(execute_external(
-		param->cmd, param->env), param);
+		exit_child_process(execute_builtin(param->cmd, &param->env,
+				param->last_exit), param);
+	exit_child_process(execute_external(param->cmd, param->env), param);
 }
 
 void	parent_process(t_process_command_args *param)
@@ -68,7 +47,7 @@ void	parent_process(t_process_command_args *param)
 	int	status;
 
 	if (!param->cmd || !(param->last_exit))
-	return ;
+		return ;
 	if (param->cmd->next && param->pipefd[0] > 0)
 	{
 		close(param->pipefd[1]);
@@ -79,28 +58,28 @@ void	parent_process(t_process_command_args *param)
 	*(param->last_exit) = WEXITSTATUS(status);
 }
 
-int	process_command(t_command *cmd,
-	char **env, int *last_exit, t_token	*tokens)
+int	process_command(t_command *cmd, char **env, int *last_exit, t_token *tokens)
 {
-	int		pipefd[2];
-	pid_t	pid;
+	int						pipefd[2];
 	t_process_command_args	args;
 
 	if (!cmd || !env || !last_exit)
 		return (-1);
 	if (setup_pipe(cmd, pipefd) < 0)
 		return (-1);
-	pid = fork();
-	args.cmd	= cmd;
-	args.env	= env;
-	args.pid	= pid;
-	args.tokens	= tokens;
-	args.last_exit	= last_exit;
-	args.pipefd[0]	= pipefd[0];
-	args.pipefd[1]	= pipefd[1];
-	if (pid < 0)
-		return (perror("fork"), -1);
-	else if (pid == 0)
+	args.cmd = cmd;
+	args.env = env;
+	args.tokens = tokens;
+	args.last_exit = last_exit;
+	args.pid = fork();
+	args.pipefd[0] = pipefd[0];
+	args.pipefd[1] = pipefd[1];
+	if (args.pid < 0)
+	{
+		perror("fork");
+		return (-1);
+	}
+	else if (args.pid == 0)
 		child_process(&args);
 	else
 		parent_process(&args);
