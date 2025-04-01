52869ab5e122fbf61208cc2ca9da595a64305a47
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-03-31 22:47:55 -0300

    refactor(builtin): modularize builtin_pwd implementation

---
+++ sources/builtin/pwd.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/31 21:13:49 by peda-cos          #+#    #+#             */
+/*   Updated: 2025/03/31 21:14:05 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "builtin.h"
+
+int	builtin_pwd(void)
+{
+	char	*cwd;
+
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
+	{
+		perror("pwd");
+		return (1);
+	}
+	ft_putendl_fd(cwd, STDOUT_FILENO);
+	free(cwd);
+	return (0);
+}
