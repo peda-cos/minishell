37e39c8ff6af1054efc66d1a4d6df3d7045e4b7e
Jonnathan Lacerda Santos <jlacerda@student.42sp.org.br>
2025-04-16 20:03:28 -0300

    fix(validator): removes unnecessary token value validate

--- sources/validator/utils.c
+++ sources/validator/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: jlacerda <jlacerda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 17:03:12 by jlacerda          #+#    #+#             */
-/*   Updated: 2025/04/14 22:39:19 by jlacerda         ###   ########.fr       */
+/*   Updated: 2025/04/16 20:02:14 by jlacerda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,13 @@ int	is_pipe(t_token *token)
 	return (token->type == PIPE);
 }
 
-int	is_invalid_token(t_token *token)
-{
-	int	is_empty_or_null;
-
-	is_empty_or_null = !token->content->value
-		|| strlen(token->content->value) == 0;
-	if (token->type == WORD && is_empty_or_null)
-		return (TRUE);
-	if (is_pipe(token) && (!token->prev || !token->next))
-		return (TRUE);
-	return (FALSE);
-}
-
 int	is_valid_pipe_position(t_token *token)
 {
 	if (!token->prev || !token->next)
 		return (FALSE);
 	if (token->prev->type != WORD)
 		return (FALSE);
-	if (token->next->type != WORD && !(is_redirection(token->next)
+	if (token->next->type != WORD && !(is_redirection(token->next->type)
 			&& token->next->next && token->next->next->type == WORD))
 		return (FALSE);
 	return (TRUE);
