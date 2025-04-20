c905624d3ce76f9b1ce3b0c366e4d900aa913ff0
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-04-20 00:22:05 -0300

    fix(tokenizer): improve error handling and memory management in word processing
    - Add null checks for memory allocations
    - Handle unclosed quote error scenario
    - Prevent token creation with uninitialized content
    - Update function signatures to improve type safety

--- sources/tokenizer/word.c
+++ sources/tokenizer/word.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   word.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlacerda <jlacerda@student.42.fr>          +#+  +:+       +#+        */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/22 13:18:28 by jlacerda          #+#    #+#             */
-/*   Updated: 2025/04/19 19:02:33 by jlacerda         ###   ########.fr       */
+/*   Updated: 2025/04/20 00:21:47 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ static char	*get_word_in_quotes(char *str, int *index, char delimiter)
 	(*index)++;
 	word_index = 0;
 	word = (char *)malloc(sizeof(char) * (ft_strlen(str + *index) + 1));
+	if (!word)
+		return (NULL);
 	while (str[*index] && !is_quote_delimiter(str, index, delimiter))
 	{
 		if (str[*index] != BACKSLASH_CHR)
@@ -27,13 +29,19 @@ static char	*get_word_in_quotes(char *str, int *index, char delimiter)
 		(*index)++;
 	}
 	word[word_index] = '\0';
-	if (str[*index] == delimiter)
-		(*index)++;
+	if (str[*index] != delimiter)
+	{
+		ft_putstr_fd("minishell: syntax error: unclosed quote\n",
+			STDERR_FILENO);
+		free(word);
+		return (NULL);
+	}
+	(*index)++;
 	return (word);
 }
 
-static t_token_content	*append_token_content(
-	t_token_content *content, t_token_word_param *param)
+static t_token_content	*append_token_content(t_token_content *content,
+		t_token_word_param *param)
 {
 	t_token_content	*iter;
 	char			*temp;
@@ -42,8 +50,10 @@ static t_token_content	*append_token_content(
 	if (param->in_quotes)
 	{
 		temp = param->word;
-		param->word = get_word_in_quotes(
-				param->str, param->str_index, param->delimiter);
+		param->word = get_word_in_quotes(param->str, param->str_index,
+				param->delimiter);
+		if (!param->word)
+			return (NULL);
 		new_content = new_token_content(ft_strdup(param->word),
 				param->delimiter == SINGLE_QUOTE_CHR);
 		free(temp);
@@ -62,8 +72,8 @@ static t_token_content	*append_token_content(
 	return (content);
 }
 
-static t_token_content	*add_only_content_word(
-	t_token_content *content, t_token_word_param *param)
+static t_token_content	*add_only_content_word(t_token_content *content,
+		t_token_word_param *param)
 {
 	if (param->word_index > 0)
 	{
@@ -73,20 +83,25 @@ static t_token_content	*add_only_content_word(
 	return (content);
 }
 
-static t_token_content	*process_word_in_quotes(
-	t_token_content *content, t_token_word_param *param
-)
+static t_token_content	*process_word_in_quotes(t_token_content *content,
+		t_token_word_param *param)
 {
 	int	malloc_word_len;
 
 	malloc_word_len = ft_strlen(param->str + *param->str_index);
 	param->delimiter = param->str[*param->str_index];
 	content = add_only_content_word(content, param);
+	if (!content && param->word_index > 0)
+		return (NULL);
 	free(param->word);
 	param->word = (char *)malloc(sizeof(char) * (malloc_word_len + 1));
+	if (!param->word)
+		return (NULL);
 	param->word_index = 0;
 	param->in_quotes = TRUE;
 	content = append_token_content(content, param);
+	if (!content)
+		return (NULL);
 	param->in_quotes = FALSE;
 	return (content);
 }
@@ -102,6 +117,8 @@ void	add_token_word(char *str, int *index, t_token **tokens)
 	param.in_quotes = FALSE;
 	param.str_index = index;
 	param.word = (char *)malloc(sizeof(char) * (ft_strlen(str + *index) + 1));
+	if (!param.word)
+		return ;
 	while (str[*index] && !ft_isspace(str[*index]) && !is_metachar(str[*index]))
 	{
 		if (str[*index] == DOT_AND_COMA_CHR)
@@ -109,6 +126,11 @@ void	add_token_word(char *str, int *index, t_token **tokens)
 		if (str[*index] == SINGLE_QUOTE_CHR || str[*index] == DOUBLE_QUOTE_CHR)
 		{
 			content = process_word_in_quotes(content, &param);
+			if (!content && param.word_index > 0)
+			{
+				free(param.word);
+				return ;
+			}
 			continue ;
 		}
 		if (str[*index] != BACKSLASH_CHR)
@@ -117,5 +139,6 @@ void	add_token_word(char *str, int *index, t_token **tokens)
 	}
 	content = add_only_content_word(content, &param);
 	free(param.word);
-	add_token(tokens, new_token(content, WORD));
+	if (content)
+		add_token(tokens, new_token(content, WORD));
 }
