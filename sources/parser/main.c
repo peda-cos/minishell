4ecb2f17cfbb2c7e2085c975f0339f69bafd71f0
Jonnathan Lacerda Santos <jlacerda@student.42sp.org.br>
2025-04-28 18:04:15 -0300

    fix(parser): fixes wrong merge performed in PR 57

--- sources/parser/main.c
+++ sources/parser/main.c
@@ -6,11 +6,28 @@
 /*   By: jlacerda <jlacerda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 14:40:39 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/04/26 18:47:12 by jlacerda         ###   ########.fr       */
+/*   Updated: 2025/04/28 17:52:00 by jlacerda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
+
+static void	set_output_redirect_file(char *value,
+	t_command	*cmd, int is_append)
+{
+	int	index;
+
+	index = cmd->amount_output_files;
+	if (index < OUTPUT_FILES_MAX_SIZE)
+	{
+		if (cmd->output_file_list[index])
+			free(cmd->output_file_list[index]);
+		cmd->output_file_list[index] = value;
+		cmd->output_file_list[index + 1] = NULL;
+		cmd->amount_output_files++;
+		cmd->append = is_append;
+	}
+}
 
 static void	process_heredoc_delim(t_token_content *content, t_command	*cmd)
 {
@@ -37,23 +54,6 @@ static void	process_heredoc_delim(t_token_content *content, t_command	*cmd)
 	else
 		here_doc_delim = ft_strdup(content->value);
 	cmd->heredoc_delim = ft_strdup(here_doc_delim);
-}
-
-static void	set_redirection_target(t_command *cmd, t_token *token,
-	t_content_params *content_params)
-{
-	int	index;
-
-	index = cmd->amount_output_files;
-	if (index < OUTPUT_FILES_MAX_SIZE)
-	{
-		if (cmd->output_file_list[index])
-			free(cmd->output_file_list[index]);
-		cmd->output_file_list[index] = value;
-		cmd->output_file_list[index + 1] = NULL;
-		cmd->amount_output_files++;
-		cmd->append = is_append;
-	}
 }
 
 static void	set_redirection_target(t_command *cmd,
