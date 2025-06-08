34cd44394fc4c1ca73c9b3b1be6e337646e4f146
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-06-08 07:27:15 -0300

    refactor: update header formatting and add export functions

--- sources/builtin/builtin.h
+++ sources/builtin/builtin.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlacerda <jlacerda@student.42.fr>          +#+  +:+       +#+        */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 14:15:36 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/05/03 15:55:05 by jlacerda         ###   ########.fr       */
+/*   Updated: 2025/06/08 07:22:24 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 # include "minishell.h"
 
-# define EXPORT_OPTIONS_CHARS	"afnp"
-# define UNSET_OPTIONS_CHARS	"fnv"
+# define EXPORT_OPTIONS_CHARS "afnp"
+# define UNSET_OPTIONS_CHARS "fnv"
 
 /* Environment management */
 char	**copy_env(char **envp);
@@ -30,13 +30,15 @@ int		builtin_pwd(void);
 int		builtin_export(char **args, char ***env);
 int		builtin_unset(char **args, char ***env);
 int		builtin_env(char **args, char **env);
-int		builtin_exit(t_process_command_args	*param);
+int		builtin_exit(t_process_command_args *param);
 
 /* Utility functions */
 int		is_valid_key(char *key);
 int		print_invalid_arg(char *arg);
 char	*get_env_value(char *key, char **envs);
 void	print_invalid_option(char *builtin, char *arg, char *arg_options);
+int		display_bash_export(char **env);
+void	sort_env_alphabetically(char **sorted_env, int count);
 
 /* CD Utility functions */
 void	update_pwd_env(char *old_pwd, char ***env);
